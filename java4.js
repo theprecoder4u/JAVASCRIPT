20cd2445b2ddd1bb24b572ea8cfcdee337c17987
Preetam Patel <preetampatel620@gmail.com>
2025-12-07 15:20:47 +0530

    stringsss operation

---
+++ java4.js
@@ -0,0 +1,24 @@
+//STRINGssss:
+let o1 = 'PREE';
+let o2 = 'TAM';
+console.log(o1+o2)
+
+
+let ans = `MY NAME IS ${o1}${o2}`;
+console.log(ans)
+
+console.log(o2.length)
+let a = 'Preetam';
+console.log(a.substring(2,5));
+
+console.log(ans.split(' '))
+
+let aa = 'HELLO \\KAISE \\HO \\SAARE \\LOG';
+console.log(aa.split('\\'))
+
+let abc = 'HELOO SAARE LOG KYA HAAL CHAAL'
+console.log(abc.join(','))
+
+
+
+
