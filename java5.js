b0b0e4f101695666c3b5811f0ea7e3d4c2a2e84d
Preetam Patel <preetampatel620@gmail.com>
2025-12-08 08:19:40 +0530

    functions and it's operation

---
+++ java5.js
@@ -0,0 +1,45 @@
+//FUNCTIONS:---
+//function defined---
+// function sayMyName(){
+//     console.log("Preetam")
+// }
+// //now to print this we need to call the function---
+// sayMyName();
+
+// function number(num){
+//     console.log("Printing number:", num)
+// }
+// number(5);
+
+// function number(num1, num2){
+//     console.log("Printing number Average:", (num1+num2)/2)
+// }
+// number(3,70);
+
+//Return Function -- that returns something to us---
+// function sum(a,b,c){
+//     let sum = a+b+c;
+//     return sum;
+// }
+// let ans = sum(1,2,3);
+// console.log("SUM:",ans);
+
+// function namee(first, last){
+//     let full = first + " " + last;
+//     return full;
+// }
+// let ans = namee("Preetam", "Patel");
+// console.log("FULL NAME:",ans);
+
+//More way to represent function---
+// const ans = function (a,b){
+//     return a*b;
+// }
+// console.log(ans(2,3));
+
+//--ARROW FUNCTIONS
+// const ans = (a,b) => {
+//     return a*b;
+// }
+// console.log(ans(2,5));
+
