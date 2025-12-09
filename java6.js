277666446a4d3697cec72f492ce5ed8194e2b56b
Preetam Patel <preetampatel620@gmail.com>
2025-12-09 21:14:50 +0530

    Arrays and Objects

--- java6.js
+++ java6.js
@@ -1,1 +1,34 @@
-//ARRAYS:-
+//OBJECTS:-
+// let obj = {
+//     name: "Preetam",
+//     age: 25,
+//     weight: 52,
+//     greet: function(){
+//         console.log("HELLOOOO");
+//     }
+// };
+// console.log(obj)
+// obj.greet();
+// console.log(typeof(obj));
+
+
+
+//ARRAYS:---
+//creation of array ---
+// let arr = [1,2,3,4,5];
+// console.log(arr);
+
+
+// //creation using array constructor ---
+// let brr = new Array('preetam', 1, true)
+// console.log(brr);
+
+
+// //type of arr and brr;
+// console.log(typeof(arr));
+// console.log(typeof(brr));
+
+
+
+
+
