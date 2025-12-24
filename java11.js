828f1ca5b16496f3ceba71b70f4eab09732ef6aa
Preetam Patel <preetampatel620@gmail.com>
2025-12-24 20:18:39 +0530

    math, date (object)

---
+++ java11.js
@@ -0,0 +1,16 @@
+//MATH :- (object)
+console.log(Math.PI);
+console.log(Math.max(60,30,50));
+console.log(Math.min(2,453,23,3));
+console.log(Math.floor(1.4));
+console.log(Math.ceil(1.9));
+console.log(Math.abs(-5));
+console.log(Math.sqrt(5));
+console.log(Math.pow(2,10)); // 2 ki power 10
+
+//DATE :- (object)
+let curr = new Date();
+console.log(curr);
+
+let date = new Date('July 13 2007 07:30:48');
+console.log(date);
