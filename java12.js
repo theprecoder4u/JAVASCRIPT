9557f1ec150243859cf7d10c176878f9bf07118f
Preetam Patel <preetampatel620@gmail.com>
2025-12-26 23:29:09 +0530

    Object Cloning

--- java12.js
+++ java12.js
@@ -1,13 +1,38 @@
-//OBJECT CLONING :-
-// -> Spread Operator (...)
+// //OBJECT CLONING :-
+// // -> Spread Operator (...)
 let src = {
     age: 12,
     wt: 68
 };
-let dest ={...src}; // (...) <- is spread operator
+// let dest ={...src}; // (...) <- is spread operator
 
-console.log("src: ", src);
-console.log("dest: ", dest);
+// console.log("src: ", src);
+// console.log("dest: ", dest);
 
 
-// -> 
+// -> assign Operator :-
+// let dest = Object.assign({}, src);
+// console.log("src: ", src);
+// console.log("dest: ", dest);
+
+// let src2 = {
+//     value: 10,
+//     name: "Preetam"
+// };
+
+// let dest = Object.assign({}, src, src2);
+// console.log("src: ", src);
+// console.log("dest: ", dest);
+
+
+// ->  Iteration Operator :-
+// let dest = {};
+
+// for(let key in src){
+//      let newkey = key;
+//      let newvalue = src[key];
+//      dest[newkey] = newvalue;
+// }
+
+// console.log("src: ", src);
+// console.log("dest: ", dest);
