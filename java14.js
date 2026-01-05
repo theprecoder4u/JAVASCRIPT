8037c132850bbfb510cfb966781bfbef5dc098b3
Preetam Patel <preetampatel620@gmail.com>
2026-01-05 22:21:27 +0530

    JS DOM/BOM/all

--- java14.js
+++ java14.js
@@ -1,1 +1,10 @@
 //JAVASCRIPT DOM MANIPULATION:-
+// DOM = Document Object Model
+// BOM = Browser Object Model
+
+// innerHTML -> get/set + element replace not possible
+// outerHTML -> get/set + element replace
+// textcontent -> all text content provide krta h
+// innertext -> text content provide krta h jo ki UI/browser pe dikh rha
+
+//
