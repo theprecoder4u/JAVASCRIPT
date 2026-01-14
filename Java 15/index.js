4418962e30a8097265516235fee76e715d160f84
Preetam Patel <preetampatel620@gmail.com>
2026-01-14 22:04:18 +0530

    htmll

--- Java 15/index.js
+++ Java 15/index.js
@@ -1,6 +1,10 @@
 let mydiv = document.querySelector('#mydiv');
 
-let newElement = document.createElement('span');
-newElement.textContent = "The Preetam"
+// let newElement = document.createElement('span');
+// newElement.textContent = "The Preetam"
 
-mydiv.insertAdjacentElement('beforebegin', newElement);
+// mydiv.insertAdjacentElement('beforebegin', newElement);
+
+let parent = document.querySelector('#mydiv');
+let child = document.querySelector('#fpara');
+parent.removeChild(child);
