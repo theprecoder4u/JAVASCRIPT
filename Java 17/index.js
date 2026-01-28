3d26a25cd8159595e4b5fd78492711fd726fe0f8
Preetam Patel <preetampatel620@gmail.com>
2026-01-28 21:43:40 +0530

    ,.,.

---
+++ Java 17/index.js
@@ -0,0 +1,19 @@
+//CODE 1
+
+for(let i = 0; i<=10000; i++){
+    let para = document.createElement('p');
+    para.textContent = "This a Para " + i;
+    document.body.appendChild(para);
+
+}
+
+
+
+
+//CODE 2
+
+
+
+
+
+
